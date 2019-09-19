1b24105ea7847f4ba4c554935270b7044ae6f7f3
Connor A. Smith <c.smith@centre.edu>
2019-09-18 21:11:31 -0400

    finish

---
+++ js.js
@@ -0,0 +1,107 @@
+var sayHi = function()
+{
+    var node = document.createElement("h1");
+    
+    node.innerText = "1. Right";
+    
+    var body = document.querySelector("body");
+    body.appendChild(node);
+    var button = document.querySelector("body");
+    button.setAttribute("onclick",null);
+    var button = document.querySelector("#firstClick");
+    
+    
+    
+    
+    
+    
+}
+var sayHello = function()
+{
+    var node = document.createElement("h1");
+        node.innerText = "1. Wrong";
+    
+    var body = document.querySelector("body");
+        body.appendChild(node);
+    var button = document.querySelector("body");
+        button.setAttribute("onclick",null);
+    var button = document.querySelector("#secondClick");
+    
+    
+    
+    
+    
+    
+}
+var abc = function()
+{
+    var node = document.createElement("h1");
+    
+    node.innerText = "2. Wrong";
+    
+    var body = document.querySelector("body");
+    body.appendChild(node);
+    var button = document.querySelector("body");
+    button.setAttribute("onclick",null);
+    var button = document.querySelector("#thirdClick");
+    
+    
+    
+    
+    
+    
+}
+var def = function()
+{
+    var node = document.createElement("h1");
+    
+    node.innerText = "2. Right";
+    
+    var body = document.querySelector("body");
+    body.appendChild(node);
+    var button = document.querySelector("body");
+    button.setAttribute("onclick",null);
+    var button = document.querySelector("#fourthClick");
+    
+    
+    
+    
+    
+    
+}
+var ghi = function()
+{
+    var node = document.createElement("h1");
+    
+    node.innerText = "3. Right";
+    
+    var body = document.querySelector("body");
+    body.appendChild(node);
+    var button = document.querySelector("body");
+    button.setAttribute("onclick",null);
+    var button = document.querySelector("#fifthClick");
+    
+    
+    
+    
+    
+    
+}
+var jkl = function()
+{
+    var node = document.createElement("h1");
+    
+    node.innerText = "3. Wrong";
+    
+    var body = document.querySelector("body");
+    body.appendChild(node);
+    var button = document.querySelector("body");
+    button.setAttribute("onclick",null);
+    var button = document.querySelector("#sixthClick");
+    
+    
+    
+    
+    
+    
+}
